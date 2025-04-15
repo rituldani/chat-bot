18173e51ac984f399acd307c75e031db4366fe4a
Ritul Dani <rituldani2003@gmail.com>
2025-04-15 14:50:52 +0530

    commit

--- clients/src/index.js
+++ clients/src/index.js
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-// import { Provider } from 'react-redux';
-// import store from './store';
+import { Provider } from 'react-redux';
+import store from './store';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { GoogleOAuthProvider } from "@react-oauth/google";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <GoogleOAuthProvider clientId="104945427915-mcnhnr3s0b6bphn4jqcflsqca7kbhtvt.apps.googleusercontent.com">
-      <App />
-      <ToastContainer position="top-right" />
-    </GoogleOAuthProvider>
-    {/* </Provider> */}
+    <Provider store={store}>
+      <GoogleOAuthProvider clientId="104945427915-mcnhnr3s0b6bphn4jqcflsqca7kbhtvt.apps.googleusercontent.com">
+        <App />
+        <ToastContainer position="top-right" />
+      </GoogleOAuthProvider>
+    </Provider>
   </React.StrictMode>
 );
 
