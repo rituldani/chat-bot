782e2642b4bdbd31fe66622fb00b502ddfadbb7a
Ritul Dani <100018102+rituldani@users.noreply.github.com>
2025-05-10 12:21:44 +0530

    Delete clients directory

--- clients/src/socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-const ENDPOINT = process.env.REACT_APP_SERVER_URL || "http://localhost:8000";
-
-const socket = io(ENDPOINT, {
-  transports: ["websocket"],
-  withCredentials: true,
-});
-
-export default socket;
